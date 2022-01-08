ad7d7429e2790394e2c0ee11052e6c0170292153
msbgod <93262973+msbgod@users.noreply.github.com>
2022-01-08 11:27:26 -0500

    Proveedores
    primera versión del programa con el ingreso de datos de los proveedores .. pendiente las pruebas unitarias y depuración

---
+++ proyecto/WindowsFormsAppProyectoMDS1/WindowsFormsAppProyectoMDS1/frmProveedores.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsAppProyectoMDS1
+{
+    public partial class Inicio : Form
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamWriter escribir = new StreamWriter(@"C:\Users\moises\Desktop\Met.Des.Soft\proyecto\Datos_del_proyecto.txt",true);
+            try
+            {
+                escribir.WriteLine("Nombre" + "\t" + "RUC" + "\t" + "Numero de Telefono" + "\t" + "Direccion" + "\t" + "Producto");
+                escribir.WriteLine(txtBnombre.Text + "\t" + txtBRUC.Text + "\t" + txtBNnTe.Text + "\t" + txtBDireccion.Text + "\t" + txtBProducto.Text);
+            }
+            catch
+            {
+                MessageBox.Show("ERROR"); 
+            }
+            escribir.Close();
+        }
+    }
+}
